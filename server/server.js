60fae7ffa8adc07cfdbc579399ed4d92e910177f
Shelo M. Paglinawan <104010831+Yole4@users.noreply.github.com>
2023-08-27 17:31:13 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ require('dotenv').config();
 // import cookieParser from 'cookie-parser';
 
 const app = express();
-const port = 3001;
+const port = process.env.DB_PORT || 3001;
 app.use(express.json());
 app.use(cookieParser());
 
