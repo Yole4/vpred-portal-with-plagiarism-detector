4f42cbb09205e57b22e41f4f9c3aa98861645d01
Shelo M. Paglinawan <104010831+Yole4@users.noreply.github.com>
2023-08-30 10:04:23 +0800

    Update SideBar.jsx

--- client/src/pages/admin side/SideBar.jsx
+++ client/src/pages/admin side/SideBar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import BackEndUrl from '../backend URL/BackEndUrl';
-import '../../../App.css';
+import '../../App.css';
 
 function SideBar() {
 
